dadf6b7e6fbda7e46dd69a55e1be6e75bca54567
s9d9n0 <antonio.sedeno@free.fr>
2024-06-22 19:49:01 +0200

    initialisation

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ app.get('/',function(req,res){
     res.send('Hello World');
 });
 
+app.get('/api/agents',function(req,res){
+    res.send([
+        {div:"gepex",nom:"SEDENO",prenom:"Antonio"},
+        {div:"gepex",nom:"BOURICAT",prenom:"Maud"},
+        {div:"gepex",nom:"GROS",prenom:"Jean-François"},
+        {div:"gepex",nom:"GUIBBERT",prenom:"Romuald"},
+        {div:"gepex",nom:"PELLETIER",prenom:"Frédéric"},
+        {div:"gepex",nom:"PINAULT",prenom:"Jean-Baptiste"},
+        {div:"gepex",nom:"VILAIN",prenom:"Frédéric"},
+    ]);
+});
+
 app.listen(3000, function(){
     console.log('Server started on port 3000');
 });
