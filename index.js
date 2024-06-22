7bf4a1e6290385c9cfea37f5250f8c68a78b1ca0
s9d9n0 <antonio.sedeno@free.fr>
2024-06-22 20:17:03 +0200

    initialisation

--- index.js
+++ index.js
@@ -3,33 +3,44 @@ const express = require("express");
 
 const app = express();
 
+const tableAgents = [
+    {div:"gepex",nom:"SEDENO",prenom:"Antonio"},
+    {div:"gepex",nom:"BOURICAT",prenom:"Maud"},
+    {div:"gepex",nom:"GROS",prenom:"Jean-François"},
+    {div:"gepex",nom:"GUIBBERT",prenom:"Romuald"},
+    {div:"gepex",nom:"PELLETIER",prenom:"Frédéric"},
+    {div:"gepex",nom:"PINAULT",prenom:"Jean-Baptiste"},
+    {div:"gepex",nom:"VILAIN",prenom:"Frédéric"},
+]
+
 app.get('/',function(req,res){
     res.send('Hello World');
 });
 
 app.get('/api/agents',function(req,res){
-    res.send([
-        {div:"gepex",nom:"SEDENO",prenom:"Antonio"},
-        {div:"gepex",nom:"BOURICAT",prenom:"Maud"},
-        {div:"gepex",nom:"GROS",prenom:"Jean-François"},
-        {div:"gepex",nom:"GUIBBERT",prenom:"Romuald"},
-        {div:"gepex",nom:"PELLETIER",prenom:"Frédéric"},
-        {div:"gepex",nom:"PINAULT",prenom:"Jean-Baptiste"},
-        {div:"gepex",nom:"VILAIN",prenom:"Frédéric"},
-    ]);
+    res.send(tableAgents);
+});
+
+app.get('/api/agents/:prenom',function(req,res){
+    console.log(req.params);
+    const parsePrenom = req.params.prenom;
+    
+    const resultPrenom = tableAgents.find((agent) => agent.prenom === parsePrenom);
+    if (!resultPrenom) return res.sendStatus(404);
+    return res.send(resultPrenom);
 });
 
 
-app.get('/checkInsee',async function makeRequest() {
-    try {
-      const response = await fetch('https://www.insee.fr/fr/accueil');
+// app.get('/checkInsee',async function makeRequest() {
+//     try {
+//       const response = await fetch('https://www.insee.fr/fr/accueil');
   
-      console.log('response.status: ', response.status); // 👉️ 200
-      console.log(response);
-    } catch (err) {
-      console.log(err);
-    }
-  });
+//       console.log('response.status: ', response.status); // 👉️ 200
+//       console.log(response);
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   });
   
 
 
